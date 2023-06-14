8c7eebd8f3a873a5873ecec82eaf824ad1cf31e0
Owen Campbell <owen.campbell@tanti.org.uk>
2023-06-14 15:50:49 +0100

    feat: add stubs for exported functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
-export function sum(num1: number, num2: number): number {
-    return num1 + num2;
-  }
+/**
+ * Represents the return value test for a time condition.
+ */
+interface TimeReturnValueTest {
+  comparator: string; // The comparator used for the test (e.g., '>=', '<', etc.).
+  value: Date; // The value against which the condition is tested.
+}
+
+/**
+ * Represents a time condition.
+ */
+interface TimeCondition {
+  method: string; // The method used for the condition.
+  returnValueTest: TimeReturnValueTest; // The return value test associated with the condition.
+}
+
+/**
+ * Retrieves the time conditions based on the provided timestamp.
+ * @param timestamp - The timestamp used to generate the time conditions.
+ * @returns The time conditions generated for the given timestamp.
+ */
+export function getConditions(timestamp: Date): TimeCondition {
+  const conditions: TimeCondition = {
+    method: 'timelock',
+    returnValueTest: {
+      comparator: '>=',
+      value: timestamp,
+    },
+  };
+  return conditions;
+}
+
+/**
+ * Encrypts the provided vote using the given proof and timestamp.
+ * @param proof - The proof associated with the vote.
+ * @param vote - The vote to be encrypted.
+ * @param timestamp - The timestamp associated with the vote.
+ * @returns The encrypted message.
+ */
+export function encryptVote(proof: string, vote: string, timestamp: Date): string {
+  const message = `${proof}${vote}`;
+  return message;
+}
+
+/**
+ * Decrypts the provided ciphertext using the given timestamp.
+ * @param ciphertext - The ciphertext to be decrypted.
+ * @param timestamp - The timestamp associated with the ciphertext.
+ * @returns The decrypted vote.
+ */
+export function decryptVote(ciphertext: string, timestamp: Date): string {
+  return ciphertext;
+}
+
